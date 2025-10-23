9a1f8bbfa56a153a9f7987b25aaefa03f7fcc9d9
Isfaque <shioktech@gmail.com>
2025-10-23 13:29:30 +0800

    Clear Contentful client credentials
    Removed space ID and access token from Contentful client configuration.

--- utils/createContentfulClient.js
+++ utils/createContentfulClient.js
@@ -2,9 +2,9 @@ const contentful = require('contentful')
 
 const client = contentful.createClient({
   // This is the space ID. A space is like a project folder in Contentful terms
-	space: "azdoe4z0judr",
+	space: "",
   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-  accessToken: "_KBfhG7Px9ZSTcXqgl3gqmDZGprLntIkVYX_v_yjrsI"
+  accessToken: ""
 });
 
 export { client } 
